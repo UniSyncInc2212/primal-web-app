d551627ddde46c7576c35346e1d061c1a6171cb7
UniSyncInc2212 <tentoesdown2212@gmail.com>
2026-09-19 00:56:21 -0500

    fix: remove duplicate translation slot import

--- src/lib/noteTranslate.ts
+++ src/lib/noteTranslate.ts
@@ -11,11 +11,6 @@ import {
   stripTranslationPlaceholders,
 } from './noteTranslateSlots';
 import {
-  restoreNoteTextForTranslation,
-  shieldNoteTextForTranslation,
-  stripTranslationPlaceholders,
-} from './noteTranslateSlots';
-import {
   NoteTranslatePrefs,
   NoteTranslateProvider,
   onDeviceTranslatorAvailable,
